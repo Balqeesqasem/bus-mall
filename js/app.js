85562c22bb3ab2bf7358a71ad7122f4e3921a004
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-03-02 23:46:52 +0200

    add chart to js

--- js/app.js
+++ js/app.js
@@ -7,34 +7,37 @@ var leftItem = document.getElementById('left_item_img');
 
 // // Array to store all obj
  var myObjectArray = [];
- var clkOneachimg =[];
- var timeImgDisplay=[];
+//  var clkOneachimg =[];
+// var timeImgDisplay=[];
  var arrayStoreImgWePic = [];
+
 // // to count time user clk on item
 var clkTimeCount = 0;
+var leftItemRandom, middleItemRandom, rightItemRandom;
 
-for(var i=0 ; i<shoppingIteam.length ; i++)
-    {
-      clkOneachimg.push(0);
-     timeImgDisplay.push(0);
+// for(var i=0 ; i<shoppingIteam.length ; i++)
+//     {
+//     clkOneachimg.push(0);
+//      timeImgDisplay.push(0);
 
-    }
-    console.log(clkOneachimg);
-console.log(timeImgDisplay);
+//     }
+//     console.log(clkOneachimg);
+// console.log(timeImgDisplay);
 
 // // constructer 
  function Buy(name ) {
     this.name = name.split('.')[0];
     this.urlImage = `img/${name}`;
-    this.clickImage =0;
-    
+    // this.clickImage =0;
+    this.timeImgDisplay= 0;
+    this.clkOneachimg = 0;
     myObjectArray.push(this);
 };
 
 function pickRandomItems(){
-    var leftItemRandom =  myObjectArray[randomNumber(0 , myObjectArray.length-1 )];
-    var middleItemRandom = myObjectArray[randomNumber(0, myObjectArray.length-1)];
-    var rightItemRandom =  myObjectArray[randomNumber(0 , myObjectArray.length-1 )];
+     leftItemRandom =  myObjectArray[randomNumber(0 , myObjectArray.length-1 )];// this variable is obj
+     middleItemRandom = myObjectArray[randomNumber(0, myObjectArray.length-1)];
+     rightItemRandom =  myObjectArray[randomNumber(0 , myObjectArray.length-1 )];
     
       while(leftItemRandom === middleItemRandom || leftItemRandom === rightItemRandom || middleItemRandom === rightItemRandom ){
         //pick another random item
@@ -73,10 +76,13 @@ console.log(arrayStoreImgWePic);
     
 
     leftItem.setAttribute('src' , leftItemRandom.urlImage);
+    leftItemRandom.timeImgDisplay+=1;
     leftItem.setAttribute('alt' , leftItemRandom.name);
     middleItem.setAttribute('src' , middleItemRandom.urlImage);
+    middleItemRandom.timeImgDisplay+=1;
     middleItem.setAttribute('alt' , middleItemRandom.name);
     rightItem.setAttribute('src' , rightItemRandom.urlImage);
+    rightItemRandom.timeImgDisplay+=1;
     rightItem.setAttribute('alt' , rightItemRandom.name);
 
    
@@ -87,14 +93,34 @@ console.log(arrayStoreImgWePic);
   }
 
   pickRandomItems();
-  console.log(myObjectArray);
-
+  // console.log(myObjectArray);
+  
   function clickImage(e){
     
-    if( e.target.id === 'left_item_img' || e.target.id === 'middle_item_img' || e.target.id === 'right_item_imgg'){
+    if( e.target.id === 'left_item_img' || e.target.id === 'middle_item_img' || e.target.id === 'right_item_img'){
         pickRandomItems();
       clkTimeCount++;
+      renderChartFuncion();
     }
+    if ( e.target.id  === 'left_item_img' ){
+      leftItemRandom.clkOneachimg+=1;
+    
+    }
+    if ( e.target.id  === 'middle_item_img'){
+      middleItemRandom.clkOneachimg+=1;
+    }
+    if ( e.target.id  === 'right_item_img' ){
+      rightItemRandom.clkOneachimg+=1;
+    }
+    // if ( e.target.alt  ===  'left_item_img'){
+    //   leftItemRandom.timeImgDisplay+=1;
+    // }
+    // if ( e.target.alt  ===  'middle_item_img'){
+    //   middleItemRandom.timeImgDisplay+=1;
+    // }
+    // if ( e.target.alt  ===  'right_item_imgg'){
+    //   rightItemRandom.timeImgDisplay+=1;
+    // }
     if(clkTimeCount === 25){
       //remove event listener
       allItem.removeEventListener('click' , clickImage);
@@ -102,29 +128,26 @@ console.log(arrayStoreImgWePic);
         var gitTag = document.getElementById('result');
         var listCreat = document.createElement('li');
         gitTag.appendChild(listCreat);
-        listCreat.textContent = `${myObjectArray[i].name} had ${clkOneachimg[i]} vots and was shown ${timeImgDisplay[i]}`;
+        listCreat.textContent = `${myObjectArray[i].name} had ${myObjectArray[i].clkOneachimg} clk and was shown ${myObjectArray[i].timeImgDisplay}`;
       }
       
       }
     
-    for(var i=0 ; i<shoppingIteam.length; i++){
-      if ( e.target.alt  ===  myObjectArray[i].name){
-        clkOneachimg[i]+=1;
-      }
-      if (myObjectArray[i].name ===leftItem.alt ){
-        timeImgDisplay[i]+=1;
-      }
-      if (myObjectArray[i].name ===middleItem.alt ){
-        timeImgDisplay[i]+=1;
-      }
-      if (myObjectArray[i].name ===rightItem.alt ){
-        timeImgDisplay[i]+=1;
-      }
+    
+      // if (myObjectArray[i].name ===leftItem.alt ){
+      //   timeImgDisplay[i]+=1;
+      // }
+      // if (myObjectArray[i].name ===middleItem.alt ){
+      //   timeImgDisplay[i]+=1;
+      // }
+      // if (myObjectArray[i].name ===rightItem.alt ){
+      //   timeImgDisplay[i]+=1;
+      // }
 
     }
-    console.log(timeImgDisplay);
+    // console.log(timeImgDisplay);
     
-  }
+  
   allItem.addEventListener('click' , clickImage);
 
 
@@ -133,7 +156,58 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
  
-
-
-
+ function renderChartFuncion(){
+   var iteamsName = [];
+   var iteamsClk = [];
+   var iteamsSelect = [];
+   for ( var i=0 ; i<myObjectArray.length; i++){
+var iteamName =myObjectArray[i].name;
+iteamsName.push(iteamName);
+var iteamClk = myObjectArray[i].clkOneachimg;
+iteamsClk.push(iteamClk);
+var iteamSelect = myObjectArray[i].clkOneachimg;
+iteamsSelect.push(iteamSelect);
+   }
+    console.log(iteamsName);
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: iteamsName  ,
+         
+          datasets: [{
+              label: '# of Votes',
+              data: iteamsClk ,
+              data: iteamsSelect ,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  }); 
   
+  }
+ 
