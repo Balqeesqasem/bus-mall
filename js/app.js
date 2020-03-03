3efdd9b1af20e08e6bec4fd093ead08ad6647383
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-03-04 01:05:33 +0200

    some CSS edits

--- js/app.js
+++ js/app.js
@@ -140,44 +140,19 @@ iteamsSelect.push(iteamSelect);
               label: '# of Votes',
               data: iteamsClk ,
               
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
+              backgroundColor: 'rgb(226, 74, 13)' 
+                  ,
+                  
+              borderColor:'rgb(226, 74, 13)',
+                  
               borderWidth: 1
           },
          {
             label: '# of item',
             data:  iteamsSelect,
             
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            backgroundColor:'rgb(26, 24, 25)',
+            borderColor:'rgb(26, 24, 25)',
             borderWidth: 1
          }]
         },
